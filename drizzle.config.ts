dbd3733ba4d37ca0539522a08ebf6fe801bb9044
loqman <loqmanalhakim.aripin@petronas.com>
2024-11-03 12:22:05 +0800

    remove env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,9 +5,4 @@ export default defineConfig({
   schema: "./app/db/schema.server.ts",
   dialect: "sqlite",
   driver: "d1-http",
-  dbCredentials: {
-    accountId: "0283b7a1dcc086c6bc0c64923ddcb482",
-    databaseId: "c183d415-5b3b-4f24-aa98-9ce8cf591a32",
-    token: "Amc_fdr9B5DM_lAptPsGqjvVPHHwaK6yxTm9tIyW",
-  },
 });
