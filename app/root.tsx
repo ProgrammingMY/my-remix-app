d6ed382408705a8230bb81aaed1426fac8742a31
loqman <loqmanalhakim.aripin@petronas.com>
2024-11-25 11:50:23 +0800

    favicon

--- app/root.tsx
+++ app/root.tsx
@@ -25,6 +25,51 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "manifest",
+    href: "/manifest.json",
+  },
+  {
+    rel: "icon",
+    href: "/favicon.ico",
+    type: "image/svg+xml",
+  },
+  {
+    rel: "icon",
+    href: "/favicon-16x16.png",
+    type: "image/png",
+    sizes: "16x16",
+  },
+  {
+    rel: "icon",
+    href: "/favicon-32x32.png",
+    type: "image/png",
+    sizes: "32x32",
+  },
+  {
+    rel: "icon",
+    href: "/favicon-96x96.png",
+    type: "image/png",
+    sizes: "96x96",
+  },
+  {
+    rel: "icon",
+    href: "/favicon-192x192.png",
+    type: "image/png",
+    sizes: "192x192",
+  },
+  {
+    rel: "icon",
+    href: "/favicon-512x512.png",
+    type: "image/png",
+    sizes: "512x512",
+  },
+  {
+    rel: "apple-touch-icon",
+    href: "/favicon-57x57.png",
+    type: "image/png",
+    sizes: "57x57",
+  },
 ];
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
@@ -42,6 +87,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#FFFFFF" />
+        <meta name="msapplication-TileColor" content="#FFFFFF" />
+        <meta name="msapplication-TileImage" content="/favicon-512x512.png" />
         <Meta />
         <Links />
       </head>
